dee071b00144953956188362aa58d31b80e90a2e
calcruikshank <59811572+calcruikshank@users.noreply.github.com>
2022-06-28 11:07:45 -0600

    Playerinput car controller

--- SlotCar/Assets/Scripts/CarController.cs
+++ SlotCar/Assets/Scripts/CarController.cs
@@ -1,18 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
+    public State state;
+    public enum State
+    {
+        Normal
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = State.Normal;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        switch (state)
+        {
+            case State.Normal:
+                break;
+        }
     }
+    void FixedUpdate()
+    {
+        switch (state)
+        {
+            case State.Normal:
+                break;
+        }
+    }
+
+
+    void OnMove(InputValue value)
+    {
+        //inputMovement = value.Get<Vector2>();
+        //lookDirection = value.Get<Vector2>();
+    }
+
+    void OnLook(InputValue value)
+    {
+       // directionToShoot = value.Get<Vector2>();
+    }
+
+    void OnFire()
+    {
+        Debug.Log("Fire");
+        //fire = true;
+    }
+
+    void OnFireReleased()
+    {
+      //  fire = false;
+    }
+}
 }
